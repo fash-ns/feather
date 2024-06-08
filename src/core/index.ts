d1c2146387a918f0cef10aff6861236c07ad9b1e
Farbod Shams <farbodshams.2000@gmail.com>
2024-06-08 11:07:49 +0330

    Added root point for core

---
+++ src/core/index.ts
@@ -0,0 +1,8 @@
+import Component from './components/Component';
+import PureComponent from './components/PureComponent';
+import Engine from './Engine';
+import JSXFacade from './JSXFacade';
+import Observer from './Observer';
+import Utils from './Utils';
+
+export { Component, Engine, JSXFacade, Observer, PureComponent, Utils };
